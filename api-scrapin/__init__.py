11ca1d88fb1b433b5b44dce0f53a1a65ba0729ee
Benjamin M. Gross <benjaminmgross@yahoo.com>
2014-06-23 01:57:52 -0400

    completed the folder structure for installable package

---
+++ api-scrapin/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+.. module:: __init__.py
+   :synopsis: initialization file for ``api-scrapin``
+
+.. moduleauthor:: Benjamin M. Gross <benjaminMgross@gmail.com>
+
+"""
+import api_scrapin
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='api-scrapin',
+      version='0.0.1',
+      description='scrapin Zs and makin Ds',
+      author='Benjamin M. Gross',
+      author_email='benjaminMgross@gmail.com',
+      url='https://github.com/benjaminmgross/api-scrapin',
+      packages=['api-scrapin'])
