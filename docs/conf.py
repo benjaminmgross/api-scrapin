b081239b0ce69034daec090c45528a013269b57b
Benjamin M. Gross <benjaminmgross@yahoo.com>
2014-06-25 04:44:37 -0400

    added the code to use [both locally and on rtfd](https://github.com/snide/sphinx_rtd_theme#using-this-theme-locally-then-building-on-read-the-docs)

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,16 @@
 import sys
 import os
 
-import sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_theme = "sphinx_rtd_theme"
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 s = os.path.abspath('../')
 sys.path.append(s)
 
